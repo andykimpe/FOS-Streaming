3dbefd88fc75f2e776e4351ce5bb5f5bc2bbec6a
Kimpe Andy <andykimpe@gmail.com>
2015-09-17 15:05:27 +0200

    Create en.php
    Create en.php original language for translation file

---
+++ lang/en.php
@@ -0,0 +1,15 @@
+<?php
+// English official language file
+// Define for Translation of FOS-Streaming Control Panel
+//------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+//-------------------------------------index.php--------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+define('TXT_USERNAME_PASSWOR_INVALID', 'Username or Password is Invalid');
+define('TXT_FOS_STREAMING_CONTROL_PANEL', 'FOS-Streaming Control Panel');
+define('TXT_LOGIN_YOUR_ACCOUNT', 'Login to your account');
+define('TXT_TYPE_USERNAME', 'type your username');
+define('TXT_TYPE_PASSWORD', 'type your password');
+define('TXT_LOGIN', 'Login');
+?>
