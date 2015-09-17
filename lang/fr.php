c818ba73f3cc53444c2609a7c3df7aba9bc3b781
Kimpe Andy <andykimpe@gmail.com>
2015-09-17 15:06:35 +0200

    Create fr.php
    Create fr.php for French translation

---
+++ lang/fr.php
@@ -0,0 +1,19 @@
+<?php
+// French Translation of FOS-Streaming Control Panel
+// Translation with html special character
+// to avoid display problem on some server
+// Traduction Française du Panneau de Control FOS-Streaming
+// Traduction avec caractére spéciaux html
+// pour éviter les problème d'affichage sur certain serveur
+//------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+//-------------------------------------index.php--------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
+define('TXT_USERNAME_PASSWOR_INVALID', 'Nom d\'Utilisateur ou Mot de Passe Invalide');
+define('TXT_FOS_STREAMING_CONTROL_PANEL', 'Panneau de Contr&ocirc;le FOS-Streaming');
+define('TXT_LOGIN_YOUR_ACCOUNT', 'Connectez-vous &agrave; votre compte');
+define('TXT_TYPE_USERNAME', 'tapez votre nom d\'utilisateur');
+define('TXT_TYPE_PASSWORD', 'tapez votre mot de passe');
+define('TXT_LOGIN', 'Connexion');
+?>
