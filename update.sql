db1ec36296a8fce92bf1a48e28c2552379dac763
Kimpe Andy <andykimpe@gmail.com>
2015-09-17 21:59:43 +0200

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `streams` ADD `tvgid` VARCHAR( 200 ) NOT NULL AFTER `streamurl3` ,
-ADD `tvglogo` VARCHAR( 200 ) NOT NULL AFTER `tvg-id` ,
-ADD `groupname` VARCHAR( 200 ) NOT NULL AFTER `tvg-logo` ;
+ADD `tvglogo` VARCHAR( 200 ) NOT NULL AFTER `tvgid` ,
+ADD `groupname` VARCHAR( 200 ) NOT NULL AFTER `tvglogo` ;
