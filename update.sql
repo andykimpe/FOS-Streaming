3f7fe203bc907c2878cbbd7527a4dada5fe605c3
Kimpe Andy <andykimpe@gmail.com>
2015-09-18 21:08:54 +0200

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `streams` ADD `tvgid` INT( 200 ) NOT NULL AFTER `streamurl3` ,
-ADD `tvglogo` INT( 200 ) NOT NULL AFTER `tvgid` ,
-ADD `tvgname` INT( 200 ) NOT NULL AFTER `tvglogo` ,
-ADD `grouptitle` INT( 200 ) NOT NULL AFTER `tvgname` ;
+ALTER TABLE `streams` ADD `tvgid` VARCHAR( 200 ) NOT NULL AFTER `streamurl3` ,
+ADD `tvglogo` VARCHAR( 200 ) NOT NULL AFTER `tvgid` ,
+ADD `tvgname` VARCHAR( 200 ) NOT NULL AFTER `tvglogo` ,
+ADD `grouptitle` VARCHAR( 200 ) NOT NULL AFTER `tvgname` ;
